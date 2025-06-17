050c7d4e5304252ec3d0a94213489229fb93c7d6
GONDIPALLIHARIPRIYA <110616145+GONDIPALLIHARIPRIYA@users.noreply.github.com>
2025-06-17 16:56:15 +0530

    Time: 0 ms (100%), Space: 40.9 MB (50.14%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,9 +1,14 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return  n>0 && (n & (n - 1)) == 0;
-        //bit manipulation is done bcz,the powers of 2 always have only one  bit set.
-         //16= 100000
-         //15= 011111 
-         //15 & 16 becomes zero
+
+        if(n==1){
+            return true;
+        }
+        if(n<=0 || n%2!=0){
+            return false;
+        }
+        return isPowerOfTwo(n/2);
+
+        
     }
 }
